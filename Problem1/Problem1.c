203d24c1d0c52f691dcea30380d0d61498d56a9f
[Louis Yeung] <louis0yeung@gmail.com>
2018-03-08 14:34:50 -0500

    Completed Problem 1, still need to add signals

--- Problem1/Problem1.c
+++ Problem1/Problem1.c
@@ -15,34 +15,37 @@ int main(int argc, char const *argv[])
         pid_t pid = fork();
         sleep(3);
         if(pid == 0){		//child of B(D)
-	        printf("PID: %d, ParentPID: %d\n D Sleeping now.\n", getpid(), getppid());        	
-            sleep(5);
+	        printf("I am process D -> PID: %d, ParentPID: %d\n D Sleeping now.\n", getpid(), getppid());        	
+            // sleep(5);
+            
             wait(NULL);
-            printf("Terminating with exit code: 12\n");
+            printf("Process D: Terminating with exit code: 12\n");
             exit(12);
         }
         else{				//first child of A(B)
-        	printf("PID: %d, ParentPID: %d\n B Sleeping now.\n", getpid(), getppid());
+        	printf("I am process B -> PID: %d, ParentPID: %d\n B Sleeping now.\n", getpid(), getppid());
             sleep(5);
             wait(NULL);
-            printf("Terminating with exit code: 9\n");            
+            printf("Process B: Terminating with exit code: 9\n");            
             exit(9);
         }
     }
     else{                   
         if(fork() == 0){    //second child of A(C)
-            printf("PID: %d, ParentPID: %d\n  C Sleeping now.\n", getpid(), getppid());
+            printf("I am process C -> PID: %d, ParentPID: %d\n  C Sleeping now.\n", getpid(), getppid());
         	sleep(5);
         	wait(NULL);
-            printf("Terminating with exit code: 8\n");        	
+            
+            printf("Process C: Terminating with exit code: 8\n");        	
         	exit(8);
         	
         }
         else{				//NODE A
-        	printf("PID: %d, ParentPID: %d\n A Sleeping now.\n", getpid(), getppid());
-            sleep(5);
+        	printf("I am process A -> PID: %d, ParentPID: %d\n A Sleeping now.\n", getpid(), getppid());
+            sleep(10);
+            
             wait(NULL);
-            printf("Terminating with exit code: 5\n");            
+            printf("Process A: Terminating with exit code: 5\n");            
             exit(5);
         }
         sleep(3);
