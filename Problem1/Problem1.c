20546eac665176e5962f2bfbd0b2a8204c7bba59
[Louis Yeung] <louis0yeung@gmail.com>
2018-03-08 14:12:48 -0500

    edited problem 1 code, need to add kill signals

--- Problem1/Problem1.c
+++ Problem1/Problem1.c
@@ -4,41 +4,70 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void returnTree(){
-	
+void signalHandler(int signum){
+	printf("Signal: %d\n", signum);
 }
-
-
 
 int main(int argc, char const *argv[])
 {
-    if(fork() == 0){
+	signal(SIGINT, signalHandler);
+	if(fork() == 0){ 
         pid_t pid = fork();
-        printf("PID: %d, ParentPID: %d\n", getpid()-27000, getppid()-27000);
-        printf("going to sleep A\n");
-        sleep(4);
-        if(pid == 0){
-        	exit(0);
-        }
-        else{
+        sleep(3);
+        if(pid == 0){		//child of B(D)
+	        printf("PID: %d, ParentPID: %d\n D Sleeping now.\n", getpid(), getppid());        	
+            sleep(5);
             wait(NULL);
-            	exit(0);
+            printf("Terminating with exit code: 12\n");
+            exit(12);
+        }
+        else{				//first child of A(B)
+        	printf("PID: %d, ParentPID: %d\n B Sleeping now.\n", getpid(), getppid());
+            sleep(5);
+            wait(NULL);
+            printf("Terminating with exit code: 9\n");            
+            exit(9);
         }
     }
-    else{
-        if(fork() == 0){
-            printf("PID: %d, ParentPID: %d\n", getpid()-27000, getppid()-27000);
-            printf("going to sleep B\n");
-        	sleep(4);
-        	exit(0);
+    else{                   
+        if(fork() == 0){    //second child of A(C)
+            printf("PID: %d, ParentPID: %d\n  C Sleeping now.\n", getpid(), getppid());
+        	sleep(5);
+        	wait(NULL);
+            printf("Terminating with exit code: 8\n");        	
+        	exit(8);
+        	
         }
-        else{
+        else{				//NODE A
+        	printf("PID: %d, ParentPID: %d\n A Sleeping now.\n", getpid(), getppid());
+            sleep(5);
             wait(NULL);
-            exit(0);
+            printf("Terminating with exit code: 5\n");            
+            exit(5);
         }
-        printf("PID: %d, ParentPID: %d\n", getpid()-27000, getppid()-27000);
-		printf("going to sleep C\n");
-        sleep(4);
+        sleep(3);
     }
-    return 0;
+
+
+
+	return 0;
+   
+    
+
+	// pid_t pid1 = fork();
+	// if (pid1 < 0)
+	// {
+	// 	perror("Error: ");
+	// }
+	// else if (pid1 == 0){
+	// 	//child process
+	// 	printf("I am: %d| my Parent is: %d\n", getpid(), getppid());
+	// 	spawnChild();
+	// }
+	// else{
+	// 	//process of parent of pid1
+	// 	printf("I am: %d| my Parent is: %d\n", getpid(), getppid());
+	// 	spawnChild();
+	// }
+
 }
