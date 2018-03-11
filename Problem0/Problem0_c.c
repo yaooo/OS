11c0e42254820af7aedd2ad4cc50ec1c71520fa8
louisyeung <ly178@rutgers.edu>
2018-03-11 13:11:05 -0400

    reupload code and problem 0c

--- Problem0/Problem0_c.c
+++ Problem0/Problem0_c.c
@@ -1,43 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stddef.h>
+#include <time.h>
 
-static int create_two_child(size_t *backtrace, size_t id, size_t i, size_t n);
 
-int main(void) {
-  // we create array to stock id
-  size_t backtrace[3];
-  size_t n = sizeof backtrace / sizeof *backtrace;
-  return create_two_child(backtrace, 1, 0, n);
+int num[1000000];
+int len = 0;
+clock_t startTime;
+double elapsedTime;
+
+// min, max, sum functions
+int min(int *x, int begin, int end) {
+    int mintemp = x[0];
+    for (int i = begin;i < end; i++) {
+        if(x[i] < mintemp) {
+            mintemp = x[i];
+        }
+    }
+    return mintemp;
 }
+int max(int *x, int begin, int end) {
+    int maxtemp = x[0];
+    for (int j = begin; j < end; j++) {
+        if(x[j] > maxtemp) {
+            maxtemp = x[j];
+        }
+    }
+    return maxtemp;
+}
+int sum(int *x, int begin, int end) {
+    int sumtemp = 0;
+    for (int k = begin; k < end; k++) {
+        sumtemp = sumtemp + x[k];
+    }
+    return sumtemp;
+}
+int main(int argc, char* argv[]){
+    FILE *fp = fopen("Input100k.txt", "r");
+    FILE *output = fopen("out.txt", "w+");
+    int count = 0;
+    int numbers; //buffer for each line
+    int fd[2];
+    pipe(fd);
 
-// tail recursive function: child will call this function to create two new
-// child
-static int create_two_child(size_t *backtrace, size_t id, size_t i, size_t n) {
-  if (i < n) {                       // we look our level
-    for (size_t j = 0; j < 2; j++) { // we loop to create two child
-      pid_t pid = fork();
-      if (pid == -1) { // error
-        perror("fork()");
-        return 1;
-      } else if (pid == 0) { // child
-        backtrace[i] = id + 1;
-        // id * 2 cause we create 2 child each time
-        return create_two_child(backtrace, id * 2, i + 1,
-                                n); // we call ourself with i + 1 and
-                                    // stop function here wich return we
-                                    // don't want that child continue the
-                                    // loop
-      }
-      id++;
-      // parent will continue the loop to the number of child wanted
+    while(fscanf(fp, "%d\n", &numbers) != EOF){
+        num[count] = numbers;
+        count++;
     }
-  } else {
-    // if we are at max level we show our backtrace
-    printf("Last Level Children: 1");
-    for (size_t j = 0; j < n; j++) {
-      printf(", %zu", backtrace[j]);
+    fclose(fp);  
+    startTime = clock();
+
+    printf("%d\n", max(num, 0, count));
+
+    int partitions = 8;
+    int currentPart = 0;
+    int levels = 3;
+    int section = count/partitions;
+
+    printf("count, section, partition, %d.%d.%d\n", count, section, partitions); //proof of concept
+
+    for (int i = 0; i < levels; ++i)
+    {
+        if (fork() == 0) //creates a random process tree 
+        {
+            printf("PID: %d, Parent PID: %d\n", getpid(), getppid());
+            minSect = min(x, currentPart*section, (currentPart+1)*section);
+            maxSect = max(x, currentPart*section, (currentPart+1)*section);
+            sumSect = sum(x, currentPart*section, (currentPart+1)*section);
+
+        }
     }
-    printf("\n");
-  }
-  return 0;
+
+    fclose(output);
+    elapsedTime = (clock() - startTime);
+    printf("Total time: %0.3f ticks from Problem 0 part b.(1 tick = 1/1000000s)\n", elapsedTime);
+    
+    return(0);
 }
--- Problem1/Problem1.c
+++ Problem1/Problem1.c
@@ -5,42 +5,42 @@
 #include <unistd.h>
 
 void explain_wait_status(pid_t pid, int status){
-	if(WIFEXITED(status)){
-		printf("Child with PID = %d terminated normally, Exit code: %d\n", pid, WEXITSTATUS(status));
-	}
-	else if (WIFSIGNALED(status)){
-		printf("Child with PID = %d terminated by a signal, Exit code: %d\n", pid, WTERMSIG(status));
-	}
-	else if (WIFSTOPPED(status)){
-		printf("Child with PID = %d has been stopped by a signal, Exit code: %d\n", pid, WSTOPSIG(status));
-	}
-	else{
-		printf("%s: Internal error: Unhandled case, PID = %d, Exit code: %d\n", __func__, pid, status);
-		exit(1);
-	}
+    if(WIFEXITED(status)){
+        printf("Child with PID = %d terminated normally, Exit code: %d\n", pid, WEXITSTATUS(status));
+    }
+    else if (WIFSIGNALED(status)){
+        printf("Child with PID = %d terminated by a signal, Exit code: %d\n", pid, WTERMSIG(status));
+    }
+    else if (WIFSTOPPED(status)){
+        printf("Child with PID = %d has been stopped by a signal, Exit code: %d\n", pid, WSTOPSIG(status));
+    }
+    else{
+        printf("%s: Internal error: Unhandled case, PID = %d, Exit code: %d\n", __func__, pid, status);
+        exit(1);
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-	int a = 0; int b = 0; int c = 0; int d = 0;
-	int status;
-	if(fork() == 0){ 
+    int a = 0; int b = 0; int c = 0; int d = 0;
+    int status;
+    if(fork() == 0){ 
         pid_t pid = fork();
         sleep(3);
-        if(pid == 0){		//child of B(D)
-        	d = getpid();
-	        printf("I am process D -> PID: %d, ParentPID: %d\n", getpid(), getppid());   
-	        printf("D Sleeping now.\n");     	
+        if(pid == 0){       //child of B(D)
+            d = getpid();
+            printf("I am process D -> PID: %d, ParentPID: %d\n", getpid(), getppid());   
+            printf("D Sleeping now.\n");        
             sleep(5);
             printf("Process D waiting now.\n");
-	        wait(&status);
+            wait(&status);
             explain_wait_status(getpid(), 12);
             exit(12);
         }
-        else{				//first child of A(B)
-        	b = getpid();
-        	printf("I am process B -> PID: %d, ParentPID: %d\n", getpid(), getppid());
-        	printf("B Sleeping now.\n");
+        else{               //first child of A(B)
+            b = getpid();
+            printf("I am process B -> PID: %d, ParentPID: %d\n", getpid(), getppid());
+            printf("B Sleeping now.\n");
             sleep(5);
             printf("Process B waiting now.\n");
             waitpid(d, &status, WUNTRACED);
@@ -51,20 +51,20 @@ int main(int argc, char const *argv[])
     }
     else{                   
         if(fork() == 0){    //second child of A(C)
-        	c = getpid();
+            c = getpid();
             printf("I am process C -> PID: %d, ParentPID: %d\n", getpid(), getppid());
             printf("C Sleeping now.\n");
-        	sleep(10);
-        	printf("Process C waiting now.\n");
-        	wait(&status);
+            sleep(10);
+            printf("Process C waiting now.\n");
+            wait(&status);
             explain_wait_status(getpid(), 8);
-        	exit(8);
+            exit(8);
         }
-        else{				//parent node A
-        	a = getpid();
-			printf("I am process A with PID: %d, Spawning child now.\n", getpid());
-        	printf("I am process A -> PID: %d, ParentPID: %d\n", getpid(), getppid());
-        	printf("A Sleeping now.\n");
+        else{               //parent node A
+            a = getpid();
+            printf("I am process A with PID: %d, Spawning child now.\n", getpid());
+            printf("I am process A -> PID: %d, ParentPID: %d\n", getpid(), getppid());
+            printf("A Sleeping now.\n");
             sleep(10);
             printf("Process A waiting now.\n");
             waitpid(b, &status, WUNTRACED);
@@ -72,5 +72,5 @@ int main(int argc, char const *argv[])
             exit(5);
         }
     }
-	return 0;
+    return 0;
 }
